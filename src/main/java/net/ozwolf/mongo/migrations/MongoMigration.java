d73972a188240b25f9278816702a4893f5f9c6a5
Wade Pearce <wade.pearce@ioof.com.au>
2014-12-09 08:56:29 +1100

    Added inherited flag to annotation.

--- src/main/java/net/ozwolf/mongo/migrations/MongoMigration.java
+++ src/main/java/net/ozwolf/mongo/migrations/MongoMigration.java
@@ -1,12 +1,10 @@
 package net.ozwolf.mongo.migrations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface MongoMigration {
     String version();
 
